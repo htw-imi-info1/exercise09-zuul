87437fc189b0905c32e9fed122b197ef1790f121
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-12-21 21:19:45 +0100

    add additions for command hierarchy refactoring

---
+++ additions_03_Command_Hierarchy/Quit.java
@@ -0,0 +1,10 @@
+public class Quit extends Command
+{
+    public Quit(CommandWord firstWord, String secondWord){
+        super(firstWord, secondWord);
+    }
+
+    public String processCommand(Player player){
+        return "Not Yet Implemented";      
+    }
+}
